df8085efb42a72c9de474c1de4934485a65b7865
Sergii Krymets <skrymets@gmail.com>
2018-01-17 01:16:21 +0200

    Add Game scene and 'Back to Start button' Add 'Start' and 'Back to Start' Buttons scripting handlers Add 'Back to Start' button model

--- Number Wizard UI Game/Assets/LevelManager.cs
+++ Number Wizard UI Game/Assets/LevelManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
     public void LoadLevel(string levelName)
     {
         Debug.Log("Load level requested for: " + levelName);
+        SceneManager.LoadScene(levelName);
     }
 
     public void RequestQuit()
