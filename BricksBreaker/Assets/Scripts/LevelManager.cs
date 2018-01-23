0944fc6630c40a48bc8ce90578305a2a51f684d3
Sergii Krymets <skrymets@intropro.com>
2018-01-23 19:51:17 +0200

    NEW BricksBreaker GAME

---
+++ BricksBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel(string levelName)
+	{
+		Debug.Log("Load level requested for: " + levelName);
+		SceneManager.LoadScene(levelName);
+	}
+
+	public void RequestQuit()
+	{
+		Debug.Log("Quit requested");
+		Application.Quit();
+	}
+
+	public void PlayerWin ()
+	{
+		SceneManager.LoadScene("PlayerWin");
+	}
+
+	public void PlayerLose ()
+	{
+		SceneManager.LoadScene("PlayerLose");
+	}
+}
