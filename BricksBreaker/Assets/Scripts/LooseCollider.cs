02832e6931ecba19fe869f07346775da30a25f75
Sergii Krymets <skrymets@intropro.com>
2018-01-24 18:17:14 +0200

    Add a platform that moves and a ball that jumps, surrounding walls as well

---
+++ BricksBreaker/Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		Debug.Log("Trigger enter");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log("Collision enter");
+	}
+
+}
---
+++ BricksBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float mousePosInBlocks = Mathf.Clamp(Input.mousePosition.x / Screen.width * 20, 0.667f, 19.333f);
+		transform.position = new Vector3(mousePosInBlocks, transform.position.y);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		collision.gameObject.GetComponent<Rigidbody2D>().velocity *= 1.1f;
+	}
+}
