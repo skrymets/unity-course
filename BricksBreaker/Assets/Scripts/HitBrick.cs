5b560cea08e5c52fe3a2c2755e548f88afee9de2
Sergii Krymets <skrymets@gmail.com>
2018-01-25 00:10:14 +0200

    Make everything a prefab

---
+++ BricksBreaker/Assets/Scripts/HitBrick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBrick : MonoBehaviour {
+
+    public int maxHits;
+
+    public int currentHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("A hit!"); 
+    }
+}
