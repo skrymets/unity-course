1374e120df9c2bd212555466639036720ea0d6ba
Sergii Krymets <skrymets@intropro.com>
2018-01-25 12:29:21 +0200

    Add some background music

---
+++ BricksBreaker/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+	static MusicPlayer _instance;
+
+	public static MusicPlayer instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				_instance = GameObject.FindObjectOfType<MusicPlayer>();
+				if (_instance == null)
+				{
+					Debug.LogWarning("Player not found");
+				}
+				else {
+					GameObject.DontDestroyOnLoad(_instance.gameObject);
+				}
+			}
+			return _instance;
+		}
+	}
+
+
+	// Use this for initialization
+	void Awake()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else {
+			Destroy(gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Number Wizard UI Game/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	private static MusicPlayer _playerInstance;
+
+	public static MusicPlayer playerInstance
+	{
+		get
+		{
+
+			if (_playerInstance == null)
+			{
+				_playerInstance = FindObjectOfType<MusicPlayer>();
+				Debug.Log("Player: " + _playerInstance.GetInstanceID());
+				DontDestroyOnLoad(_playerInstance);
+			}
+
+			return _playerInstance;
+		}
+	}
+
+	void Awake()
+	{
+		if (_playerInstance == null)
+		{
+			_playerInstance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+	public void ToggleMuteMusic()
+	{
+		AudioSource music = MusicPlayer.playerInstance.gameObject.GetComponent<AudioSource>();
+		music.mute = !music.mute;
+	}
+
+}
