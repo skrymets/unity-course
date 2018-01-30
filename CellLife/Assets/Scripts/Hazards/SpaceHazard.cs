ba15cb48f86875a97366d80a14988493a6b696f9
Sergii Krymets <skrymets@gmail.com>
2018-01-30 18:33:50 +0200

    Start implementing a multiple hazards types

---
+++ CellLife/Assets/Scripts/Hazards/SpaceHazard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpaceHazard : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = Vector3.down * speed;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, .5f);
+    }
+}
